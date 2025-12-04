d68967b9e299904d53db2a021c04e156d859a564
Jordan B. <jordanbrown2622@gmail.com>
2025-12-04 17:09:06 -0500

    brb pushing to main

--- backend/src/main/resources/db/migration/V6__set_series_cascade.sql
+++ backend/src/main/resources/db/migration/V6__set_series_cascade.sql
@@ -1,3 +1,3 @@
 ALTER TABLE series DROP FOREIGN KEY series_ibfk_1;
 ALTER TABLE series ADD CONSTRAINT fk_series_content_cascade
-    FOREIGN KEY (content_id) REFERENCES content(content_id) ON DELETE CASCADE;
+FOREIGN KEY (content_id) REFERENCES content(content_id) ON DELETE CASCADE;
