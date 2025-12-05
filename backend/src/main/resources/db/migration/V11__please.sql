015a27bf2c9f7733dbd79d37b20a9e392c22059b
Jordan B. <jordanbrown2622@gmail.com>
2025-12-04 20:40:05 -0500

    pls

---
+++ backend/src/main/resources/db/migration/V11__please.sql
@@ -0,0 +1,19 @@
+ALTER TABLE member DROP FOREIGN KEY member_ibfk_1;
+ALTER TABLE member ADD CONSTRAINT fk_member_plan_cascade
+    FOREIGN KEY(subscription_id) REFERENCES subscriptionPlan(subscription_id) ON DELETE CASCADE;
+
+ALTER TABLE castIn DROP FOREIGN KEY castIn_ibfk_2;
+ALTER TABLE castIn ADD CONSTRAINT fk_castIn_person_cascade
+    FOREIGN KEY (person_id) REFERENCES person(person_id) ON DELETE CASCADE;
+
+ALTER TABLE directedBy DROP FOREIGN KEY directedBy_ibfk_2;
+ALTER TABLE directedBy ADD CONSTRAINT fk_directedBy_person_cascade
+    FOREIGN KEY (person_id) REFERENCES person(person_id) ON DELETE CASCADE;
+
+ALTER TABLE awardedTo DROP FOREIGN KEY awardedTo_ibfk_1;
+ALTER TABLE awardedTo ADD CONSTRAINT fk_awardedTo_award_cascade
+    FOREIGN KEY (award_name) REFERENCES award(award_name) ON DELETE CASCADE;
+
+ALTER TABLE has DROP FOREIGN KEY has_ibfk_3;
+ALTER TABLE has ADD CONSTRAINT fk_has_history_cascade
+    FOREIGN KEY (stream_id) REFERENCES streamingHistory(stream_id) ON DELETE CASCADE;
